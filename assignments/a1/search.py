85e2b0a435d0375626a37005a48dda202e454558
Silvan Adrian <hallo@silvanadrian.ch>
2019-09-25 16:17:35 +0800

    init ass1

---
+++ assignments/a1/search.py
@@ -0,0 +1,184 @@
+"""
+In search.py, you will implement generic search algorithms which are called
+by Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples,
+        (successor, action, stepCost), where 'successor' is a
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental
+        cost of expanding to that successor
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        util.raiseNotDefined()
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s,s,w,s,w,w,s,w]
+
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first
+
+    Your search algorithm needs to return a list of actions that reaches
+    the goal.  Make sure to implement a graph search algorithm
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    """
+    "*** YOUR CODE HERE ***"
+    from game import Directions
+    frontier = util.Stack()
+    directions = util.Stack()
+    frontier.push([problem.getStartState()])
+    directions.push([])
+    exploredSet = set()
+    while frontier:
+        node = frontier.pop()
+        dirc = directions.pop()
+        if (problem.isGoalState(node[-1])):
+            return dirc
+        if node[-1] not in exploredSet:
+            #print('Exploring:', node[-1], '...')
+            exploredSet.add(node[-1])
+            for child in problem.getSuccessors(node[-1]):
+                temp1 = list(node)
+                temp1.append(child[0])
+                frontier.push(temp1)
+                temp2 = list(dirc)
+                temp2.append(child[1])
+                directions.push(temp2)
+
+def breadthFirstSearch(problem):
+    """
+    Search the shallowest nodes in the search tree first.
+    """
+    "*** YOUR CODE HERE ***"
+    from game import Directions
+    frontier = util.Stack()
+    directions = util.Stack()
+    frontier.push([problem.getStartState()])
+    directions.push([])
+    exploredSet = set()
+    while frontier:
+        node = frontier.pop()
+        dirc = directions.pop()
+        if (problem.isGoalState(node[-1])):
+            return dirc
+        if node[-1] not in exploredSet:
+            #print('Exploring:', node[-1], '...')
+            exploredSet.add(node[-1])
+            for child in problem.getSuccessors(node[-1]):
+                temp1 = list(node)
+                temp1.append(child[0])
+                frontier.push(temp1)
+                temp2 = list(dirc)
+                temp2.append(child[1])
+                directions.push(temp2)
+
+def uniformCostSearch(problem):
+    "Search the node of least total cost first. "
+    "*** YOUR CODE HERE ***"
+    from game import Directions
+    frontier = util.Queue()
+    directions = util.Queue()
+    frontier.push([problem.getStartState()])
+    directions.push([])
+    exploredSet = []
+    while frontier:
+        node = frontier.pop()
+        dirc = directions.pop()
+        if (problem.isGoalState(node[-1])):
+            return dirc
+        if node[-1] not in exploredSet:
+            #print('Exploring:', node[-1], '...')
+            exploredSet.append(node[-1])
+            for child in problem.getSuccessors(node[-1]):
+                temp1 = list(node)
+                temp1.append(child[0])
+                frontier.push(temp1)
+                temp2 = list(dirc)
+                temp2.append(child[1])
+                directions.push(temp2)
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    "Search the node that has the lowest combined cost and heuristic first."
+    "*** YOUR CODE HERE ***"
+    from heapq import heappush, heappop
+    frontier = []
+    heappush(frontier, (heuristic(problem.getStartState(), problem), [problem.getStartState()], []))
+    exploredSet = []
+    while frontier:
+        node = heappop(frontier)
+        if (problem.isGoalState(node[1][-1])):
+            return node[2]
+        if node[1][-1] not in exploredSet:
+            #print('Exploring:', node[1][-1], 'at cost', node[0])
+            exploredSet.append(node[1][-1])
+            for child in problem.getSuccessors(node[1][-1]):
+                temp1 = list(node[1])
+                temp1.append(child[0])
+                temp2 = list(node[2])
+                temp2.append(child[1])
+                heappush(frontier, ((node[0] + child[2] - heuristic(node[1][-1], problem) + heuristic(child[0], problem)), temp1, temp2))
+                #print("Added node:", child[0], "Added direction:", child[1], "Added cost:", heuristic(child[0], problem))
+            #print(exploredSet)
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
---
+++ assignments/a1/solveEightQueens.py
@@ -0,0 +1,126 @@
+import random
+import copy
+from optparse import OptionParser
+import util
+
+class SolveEightQueens:
+    def __init__(self, numberOfRuns, verbose, lectureExample):
+        """
+        Value 1 indicates the position of queen
+        """
+        self.numberOfRuns = numberOfRuns
+        self.verbose = verbose
+        self.lectureCase = [[]]
+        if lectureExample:
+            self.lectureCase = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+    def solve(self):
+        solutionCounter = 0
+        for i in range(self.numberOfRuns):
+            if self.search(Board(self.lectureCase), self.verbose).getNumberOfAttacks() == 0:
+                solutionCounter += 1
+        print("Solved: %d/%d" % (solutionCounter, self.numberOfRuns))
+
+    def search(self, board, verbose):
+        """
+        Hint: Modify the stop criterion in this function
+        """
+        newBoard = board
+        i = 0 
+        while True:
+            if verbose:
+                print("iteration %d" % i)
+                print(newBoard.toString())
+                print("# attacks: %s" % str(newBoard.getNumberOfAttacks()))
+                print(newBoard.getCostBoard().toString(True))
+            currentNumberOfAttacks = newBoard.getNumberOfAttacks()
+            (newBoard, newNumberOfAttacks, newRow, newCol) = newBoard.getBetterBoard()
+            i += 1
+            if currentNumberOfAttacks <= newNumberOfAttacks:
+                break
+        return newBoard
+
+class Board:
+    def __init__(self, squareArray = [[]]):
+        if squareArray == [[]]:
+            self.squareArray = self.initBoardWithRandomQueens()
+        else:
+            self.squareArray = squareArray
+
+    @staticmethod
+    def initBoardWithRandomQueens():
+        tmpSquareArray = [[ 0 for i in range(8)] for j in range(8)]
+        for i in range(8):
+            tmpSquareArray[random.randint(0,7)][i] = 1
+        return tmpSquareArray
+          
+    def toString(self, isCostBoard=False):
+        """
+        Transform the Array in Board or cost Board to printable string
+        """
+        s = ""
+        for i in range(8):
+            for j in range(8):
+                if isCostBoard: # Cost board
+                    cost = self.squareArray[i][j]
+                    s = (s + "%3d" % cost) if cost < 9999 else (s + "  q")
+                else: # Board
+                    s = (s + ". ") if self.squareArray[i][j] == 0 else (s + "q ")
+            s += "\n"
+        return s 
+
+    def getCostBoard(self):
+        """
+        First Initalize all the cost as 9999. 
+        After filling, the position with 9999 cost indicating the position of queen.
+        """
+        costBoard = Board([[ 9999 for i in range(8)] for j in range(8)])
+        for r in range(8):
+            for c in range(8):
+                if self.squareArray[r][c] == 1:
+                    for rr in range(8):
+                        if rr != r:
+                            testboard = copy.deepcopy(self)
+                            testboard.squareArray[r][c] = 0
+                            testboard.squareArray[rr][c] = 1
+                            costBoard.squareArray[rr][c] = testboard.getNumberOfAttacks()
+        return costBoard
+
+    def getBetterBoard(self):
+        """
+        "*** YOUR CODE HERE ***"
+        This function should return a tuple containing containing four values
+        the new Board object, the new number of attacks, 
+        the Column and Row of the new queen  
+        For exmaple: 
+            return (betterBoard, minNumOfAttack, newRow, newCol)
+        The datatype of minNumOfAttack, newRow and newCol should be int
+        """
+        util.raiseNotDefined()
+
+    def getNumberOfAttacks(self):
+        """
+        "*** YOUR CODE HERE ***"
+        This function should return the number of attacks of the current board
+        The datatype of the return value should be int
+        """
+        util.raiseNotDefined()
+
+if __name__ == "__main__":
+    #Enable the following line to generate the same random numbers (useful for debugging)
+    random.seed(1)
+    parser = OptionParser()
+    parser.add_option("-q", dest="verbose", action="store_false", default=True)
+    parser.add_option("-l", dest="lectureExample", action="store_true", default=False)
+    parser.add_option("-n", dest="numberOfRuns", default=1, type="int")
+    (options, args) = parser.parse_args()
+    EightQueensAgent = SolveEightQueens(verbose=options.verbose, numberOfRuns=options.numberOfRuns, lectureExample=options.lectureExample)
+    EightQueensAgent.solve()
